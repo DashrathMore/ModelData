3944d6b56d32bff659557d147867605c313451b3
DashrathMore <dashrathmore990@gmail.com>
2023-08-17 12:21:53 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class DEPT(models.Model):
+    DEPTNO=models.IntegerField(primary_key=True)
+    DNAME=models.CharField(max_length=100)
+    LOC=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.DNAME
+
+class EMP(models.Model):
+    EMPNO=models.IntegerField(primary_key=True)
+    ENAME=models.CharField(max_length=100)
+    JOB=models.CharField(max_length=100)
+    SAL=models.IntegerField()
+    COMM=models.IntegerField(null=True, blank=True)
+    HIREDATE=models.DateField()
+    DEPTNO=models.ForeignKey(DEPT, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ENAME
